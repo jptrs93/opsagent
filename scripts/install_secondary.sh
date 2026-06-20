8ebc9f166bca1b0d8a8e433f364760fcfd996cef
Joss Peters <jptrs93@gmail.com>
2026-06-21 09:53:38 +1000

    Bump secondary install default version

--- scripts/install_secondary.sh
+++ scripts/install_secondary.sh
@@ -20,7 +20,7 @@ detect_arch() {
 }
 
 requested_version() {
-    local version="${OPENDEPLOY_VERSION:-v0.0.160}"
+    local version="${OPENDEPLOY_VERSION:-v0.0.202}"
     while [ "$#" -gt 0 ]; do
         case "$1" in
             --version=*)
