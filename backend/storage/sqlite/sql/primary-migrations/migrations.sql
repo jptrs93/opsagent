1eb897d1d8455bc602eb66f824105aeb32bf99cd
Joss Peters <jptrs93@gmail.com>
2026-07-20 17:19:49 +1000

    ui tweaks

--- backend/storage/sqlite/sql/primary-migrations/migrations.sql
+++ backend/storage/sqlite/sql/primary-migrations/migrations.sql
@@ -1,7 +1,1 @@
-CREATE UNIQUE INDEX IF NOT EXISTS idx_deployment_configs_active_node_identity
-    ON deployment_configs(node_id, space_id, name)
-    WHERE deleted = 0;
-
-DROP INDEX IF EXISTS idx_deployment_configs_active_identity;
-DROP INDEX IF EXISTS idx_deployment_configs_identity;
-ALTER TABLE deployment_configs DROP COLUMN machine;
+-- Add migrations here
